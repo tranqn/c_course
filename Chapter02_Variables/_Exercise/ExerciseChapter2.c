945acf85aa29280a1755b7d7361874920c83da3a
Quoc Nam Tran <namquoctran24@gmail.com>
2024-07-24 16:24:48 +0200

    finished Chapter2

--- Chapter02_Variables/_Exercise/ExerciseChapter2.c
+++ Chapter02_Variables/_Exercise/ExerciseChapter2.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    u_int64_t age;
+    u_int64_t days;
+    u_int64_t hours;
+    u_int64_t minutes;
+    u_int64_t seconds;
 
+    printf("Please enter your age in years: ");
+    scanf("%llu/n", &age);
+
+    days = age * 365;
+    hours = days * 24;
+    minutes = hours * 60;
+    seconds = minutes * 60;
+
+    printf("You are living since %llu days\n", days);
+    printf("You are living since %llu hours\n", hours);
+    printf("You are living since %llu minutes\n", minutes);
+    printf("You are living since %llu seconds\n", seconds);
 
     return 0;
 }
