f086a2e0e066c530dcec6c9de2c4e318923abaed
Quoc Nam Tran <namquoctran24@gmail.com>
2024-09-13 20:10:26 +0200

    repeated Chatper04

---
+++ Chapter04_Loops/_Exercise/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void print_board(int num_cols, int num_rows)
+{
+
+    for (int row = 0; row < num_rows; ++row)
+    {
+        for(int col = 0; col < num_cols; ++col)
+        {
+            if(col == 0)
+            {
+                printf("<\t");
+                continue;
+            }
+            else if(col == num_cols-1)
+            {
+                printf(">");
+            }
+            else if(row == 0)
+            {
+                printf("  ^\t");
+                continue;
+            }
+            else if(row == num_rows-1)
+            {
+                printf("  v\t");
+            }
+            else
+            {
+                printf("(%d,%d)\t", col, row);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    print_board(6,6);
+}
